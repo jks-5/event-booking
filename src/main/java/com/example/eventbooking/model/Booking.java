ea38f9e7ab998275f2e1506b5bf9361b7e4f43cc
julian <suskaj8@gmail.com>
2026-08-20 18:07:56 +0200

    add JPA entities for event, user, booking

---
+++ src/main/java/com/example/eventbooking/model/Booking.java
@@ -0,0 +1,73 @@
+package com.example.eventbooking.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UniqueEventIdUserId", columnNames = {"user_id", "event_id"})
+})
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
+    private User user;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "event_id", nullable = false)
+    @NotNull
+    private Event event;
+    @NotNull
+    private OffsetDateTime registeredAt;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Status status;
+
+
+    protected Booking() {
+
+    }
+
+    public enum Status {CONFIRMED, CANCELLED}
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public OffsetDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(OffsetDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
